e891ba9aacf5d3a51a3bc346a6682f7f127a980b
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 00:24:42 +0100

    fix: typos in part and mismatched variable names

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -4,13 +4,13 @@ def split_nodes_delimeter(old_nodes, delimeter, text_type):
         if old_node.text_type is not TextType.TEXT:
             new_nodes.append(old_node)
         else:
-            splitted_node = old_node.text.split(delimeter)
+            parts = old_node.text.split(delimeter)
             if len(parts) % 2 == 0:
                 raise Exception("invalid markdown syntax")
             for index, part in enumerate(parts):
                 if index % 2 == 0:
                     node = TextNode(part, TextType.TEXT)
                 else:
-                    node = TextNode(patrt, text_type)
+                    node = TextNode(part, text_type)
                 
                 new_nodes.append(node)
