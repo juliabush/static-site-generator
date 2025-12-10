85e06f34376ce9ef6ad84e07070a9b3f44d72462
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 00:22:26 +0100

    update: fixing the logic in the conditionals of the split_nodes_delimeter function

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -4,10 +4,13 @@ def split_nodes_delimeter(old_nodes, delimeter, text_type):
         if old_node.text_type is not TextType.TEXT:
             new_nodes.append(old_node)
         else:
-                 # 1. split the text
-        # 2. check for valid delimiter pairs
-        # 3. build new TextNodes and append to new_nodes
-            splitted_node = old_node.text.split(delimiter)
-            for index, part in enumerate(splitted_node):
-                new_text_node = TextNode(splits)
-        raise Exception("invalid markdown syntax")
+            splitted_node = old_node.text.split(delimeter)
+            if len(parts) % 2 == 0:
+                raise Exception("invalid markdown syntax")
+            for index, part in enumerate(parts):
+                if index % 2 == 0:
+                    node = TextNode(part, TextType.TEXT)
+                else:
+                    node = TextNode(patrt, text_type)
+                
+                new_nodes.append(node)
