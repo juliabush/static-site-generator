1341698d5ad3574f32e061d31e62bb04e1401f1f
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 16:19:54 +0100

    add: defining a new textNodeObject and printing it to screen

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    textNodeObject = TextNode("This is some anchor text", TextType.LINK , "https://www.boot.dev")
+    print(textNodeObject)
+
+
+main()
