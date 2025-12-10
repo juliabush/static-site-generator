bcbade4c1e184e86bdc24142115f384a97c05318
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 16:20:57 +0100

    fix: removing colon at the end of return statement

--- src/textnode.py
+++ src/textnode.py
@@ -16,7 +16,7 @@ class TextNode:
     def __eq__(self, other):
         if not isinstance(other, TextNode):
             return False
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
