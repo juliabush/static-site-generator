150453119b17eb03fe902f4d95a4d6d529f3b594
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 00:13:22 +0100

    update: adding splitted node line into else conditional

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -3,8 +3,8 @@ def split_nodes_delimeter(old_nodes, delimeter, text_type):
     for old_node in old_nodes:
         if old_node.text_type is not TextType.TEXT:
             new_nodes.append(old_node)
-            splitted_node = old_node.text.split(delimiter)
         else:
+            splitted_node = old_node.text.split(delimiter)
             for splits in splitted_node:
                 new_text_node = TextNode(splits)
         raise Exception("invalid markdown syntax")
