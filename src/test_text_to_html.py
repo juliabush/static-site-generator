26f4af9e0c2813418b4c3121a954ec49030cea37
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 22:23:35 +0100

    add: filling out unittest for text to html

---
+++ src/test_text_to_html.py
@@ -0,0 +1,5 @@
+def test_text(self):
+    node = TextNode("This is a text node", TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, None)
+    self.assertEqual(html_node.value, "This is a text node")
