b8a5d8f4bf2351884a45ddcbeca1990391d569ef
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 16:11:11 +0100

    add: export keyword to classes

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-class TextType(Enum):
+export class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
@@ -8,7 +8,7 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
-class TextNode:
+export class TextNode:
     def __init__(self, text, text_type, url):
         self.text = text
         self.text_type = text_type
