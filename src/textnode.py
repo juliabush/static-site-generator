d9604059822a0fce0d456e465c7e9c090736cfcb
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 16:18:17 +0100

    fix: removing export keywords(javascript vs python syntax), fixing indentation

--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,13 @@
 from enum import Enum
 
-export class TextType(Enum):
+class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
-
-export class TextNode:
+class TextNode:
     def __init__(self, text, text_type, url):
         self.text = text
         self.text_type = text_type
@@ -17,9 +16,7 @@ export class TextNode:
     def __eq__(self, other):
         if not isinstance(other, TextNode):
             return False
-        else:
-            if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
-               return True
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url:
 
     def __repr__(self):
-    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
