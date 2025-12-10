fc6d37f4b511813519e77760c3d881748180642d
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 23:10:34 +0100

    add: necessary import for leafnode and more descriptive valueerror

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -22,11 +24,8 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 def text_node_to_html_node(text_node):
-    if text_node.text_type not in TextType:
-        raise Exception
-    else:
         if text_node.text_type == TextType.TEXT:
-            return LeafNode(text_node.text)
+            return LeafNode(None, text_node.text)
         if text_node.text_type == TextType.BOLD:
             return LeafNode("b", text_node.text)
         if text_node.text_type == TextType.ITALIC:
@@ -34,6 +33,7 @@ def text_node_to_html_node(text_node):
         if text_node.text_type == TextType.CODE:
             return LeafNode("code", text_node.text)
         if text_node.text_type == TextType.LINK:
-            return LeafNode("a", text_node.text, "href")
+            return LeafNode("a", text_node.text, {"href": text_node.url})
         if text_node.text_type == TextType.IMAGE:
-            return LeafNode("img", "", {"src": "alt"})
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
