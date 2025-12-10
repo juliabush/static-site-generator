a38ce7372136df88bd5ed3b0847dbb1e3646f71d
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 16:08:30 +0100

    fixes: ange of fixes, mainly syntax errors like missing colons or using and vs &&

--- src/textnode.py
+++ src/textnode.py
@@ -1,25 +1,25 @@
-from enum inport Enum
+from enum import Enum
 
 class TextType(Enum):
-    PLAIN_FORMAT = ""
-    BOLD_FORMAT = "**Bold Text**"
-    ITALIC_FORMAT = "_Italic Text_"
-    CODE_FORMAT = "`Code text`"
-    LINK_FORMAT = "[anchor text](url)"
-    IMAGE_FORMAT = "![alt text](url)"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text, text_type, url):
         self.text = text
-        self.text_type = TextType
+        self.text_type = text_type
         self.url = url 
     
-    def __eq__(self, other)
-    if not isinstance(other, TextNode):
-        return False
-    else:
-        return True
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        else:
+            if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+               return True
 
-    def __repr__(self)
-    printNode = new TextNode()
-    return f"textNode({this.text, this.text_type, this.url})"
+    def __repr__(self):
+    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
