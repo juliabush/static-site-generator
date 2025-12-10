a2a8e2d163c2c53cb27258896e06d38d49d1c935
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 16:57:54 +0100

    add: 4 more tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,10 +4,30 @@ from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
+    def test_eq_bold(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    
+    def test_eq_code(self):
+        node = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_link_image(self):
+        node = TextNode("This is a text node", TextType.LINK)
+        node2 = TextNode("This is a text node", TextType.IMAGE)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_bold_italic(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url_vs_none(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://asktarotanything.com")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
 
 
 if __name__ == "__main__":
