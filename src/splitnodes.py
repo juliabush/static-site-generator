23ddddb27f4ed40037a25c21a7628c10a2d7f217
Julia Bush <j.elizabethbush@gmail.com>
2025-12-10 23:56:26 +0100

    add: creating a new empty list and appending the old_node

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,4 +1,6 @@
 def split_nodes_delimeter(old_nodes, delimeter, text_type):
+    new_list = []
     for old_node in old_nodes:
         if old_node is not TextType.TEXT:
-            
+            new_list.append(old_node)
+            raise Exception("invalid markdown syntax")
