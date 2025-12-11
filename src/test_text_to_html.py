b01265ef7119891cd2fa2ba0c005819081a442b1
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 10:33:22 +0100

    add: necessary conditional for unittest to run

--- src/test_text_to_html.py
+++ src/test_text_to_html.py
@@ -7,3 +7,6 @@ def test_text(self):
     html_node = text_node_to_html_node(node)
     self.assertEqual(html_node.tag, None)
     self.assertEqual(html_node.value, "This is a text node")
+
+if __name__ == "__main__":
+    unittest.main()
