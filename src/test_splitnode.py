8418bd61b6032036e9c0567b8a735583094b89dd
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 10:32:09 +0100

    add: defining a test_split function for unittest

--- src/test_splitnode.py
+++ src/test_splitnode.py
@@ -1,3 +1,11 @@
 import unittest
 
 from splitnodes import split_nodes_delimeter
+
+def test_split(self):
+       node = TextNode("This is a **text** node", TextType.BOLD)
+       split_node = split_nodes_delimiter(node, "**", TextType.BOLD)
+       self.assertEqual(split_node, node)
+
+if __name__ == "__main__":
+    unittest.main()
