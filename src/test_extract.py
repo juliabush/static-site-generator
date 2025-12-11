1c62520e33783606676bb8d6fbba7e43c9e44b09
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 12:32:49 +0100

    fix: adjusting paenthesis to be under matches

--- src/test_extract.py
+++ src/test_extract.py
@@ -4,5 +4,5 @@ class TestExtractMarkdown(unittest.TestCase):
     def test_extract_markdown_images(self):
         matches = extract_markdown_images(
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
-    )
+        )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
