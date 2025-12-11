9862bcafd5bd73dafa28c6c3f95057c300ef1e37
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 12:22:48 +0100

    fix: adding a missing colon

--- src/test_extract.py
+++ src/test_extract.py
@@ -1,6 +1,6 @@
 import unittest
 
-class TestExtractMarkdown(unittest.TestCase)
+class TestExtractMarkdown(unittest.TestCase):
 def test_extract_markdown_images(self):
     matches = extract_markdown_images(
         "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
