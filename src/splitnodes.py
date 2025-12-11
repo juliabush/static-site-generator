7ed17cb17f5984e5b0b2f7f27c6eadeb633e112e
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 10:26:41 +0100

    add: filling out the split nodes delimiter function

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,16 +1,21 @@
-def split_nodes_delimeter(old_nodes, delimeter, text_type):
+def split_nodes_delimiter(old_nodes, delimiter, text_type)
     new_nodes = []
     for old_node in old_nodes:
+        single_node = []
         if old_node.text_type is not TextType.TEXT:
-            new_nodes.append(old_node)
+            single_node.extend(old_node)
+            continue
         else:
-            parts = old_node.text.split(delimeter)
+            parts = old_node.text.split(delimiter)
             if len(parts) % 2 == 0:
                 raise Exception("invalid markdown syntax")
             for index, part in enumerate(parts):
+                if part == "":
+                    continue
                 if index % 2 == 0:
                     node = TextNode(part, TextType.TEXT)
                 else:
                     node = TextNode(part, text_type)
                 
-                new_nodes.append(node)
+                new_nodes.append(single_node)
+        return new_nodes
