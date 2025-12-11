caddb05724f82d29ea21c2016a9403e9ff3cdb8d
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 10:43:49 +0100

    fix:  leafnode unit test has class

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -2,9 +2,10 @@ import unittest
 
 from htmlnode import LeafNode
 
-def test_leaf_to_html_p(self):
-    node = LeafNode("p", "Hello, world!")
-    self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 if __name__ == "__main__":
     unittest.main()
