a63a99458de7b6d4714f75e55296d23d79abdcde
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 16:43:14 +0100

    update: appending next nodes after

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,4 +1,5 @@
 from textnode import TextType, TextNode
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -23,39 +24,34 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 def split_nodes_image(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
-        if old_node is not TextType.IMAGE:
+        if old_node.text_type is not TextType.TEXT:
             new_nodes.append(old_node)
         else:
-            parts = old_node.text.split()
-            if len(parts) % 2 == 0:
-                raise Exception("invalid markdown syntax")
-            for index, part in enumerate(parts):
-                if part == "":
-                    continue
-                if index % 2 == 0:
-                    node = TextNode(part, TextType.TEXT)
-                    new_nodes.append(node)
-                else:
-                    node = TextNode(part, TextType.IMAGE)
-                    new_nodes.append(node)
+           text = old_node.text
+           images = extract_markdown_images(text)
+           for alt, url in images:
+            markdown = f"![{alt}]({url})"
+            before, after = text.split(markdown, 1)
+            if before != "":
+                new_nodes.append(TextNode(before, TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+            text = after
     return new_nodes
 
 def split_nodes_link(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
-        if old_node is not TextType.LINK:
+        if old_node.text_type is not TextType.TEXT:
             new_nodes.append(old_node)
         else:
-            parts = old_node.text.split()
-            if len(parts) % 2 == 0:
-                raise Exception("invalid markdown syntax")
-            for index, part in enumerate(parts):
-                if part == "":
-                    continue
-                if index % 2 == 0:
-                    node = TextNode(part, TextType.TEXT)
-                    new_nodes.append(node)
-                else:
-                    node = TextNode(part, TextType.LINK)
-                    new_nodes.append(node)
+           text = old_node.text
+           images = extract_markdown_links(text)
+           for alt, url in images:
+            markdown = f"[{alt}]({url})"
+            before, after = text.split(markdown, 1)
+            if before != "":
+                new_nodes.append(TextNode(before, TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.LINK, url))
+            text = after
+            new_nodes.append(TextNode(after, TextType.TEXT))
     return new_nodes
