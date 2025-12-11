e1c7248db1e2cef0571c6055af367eda5254f786
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 10:42:54 +0100

    add: completed testNodedelimiter class

--- src/test_splitnode.py
+++ src/test_splitnode.py
@@ -1,11 +1,19 @@
 import unittest
 
-from splitnodes import split_nodes_delimeter
+from splitnodes import split_nodes_delimiter
+from textnode import TextNode, TextType
 
-def test_split(self):
-       node = TextNode("This is a **text** node", TextType.BOLD)
-       split_node = split_nodes_delimiter(node, "**", TextType.BOLD)
-       self.assertEqual(split_node, node)
+class TestSplitNodeDelimiter(unittest.TestCase):
+    def test_split(self):
+       node = TextNode("This is a **text** node", TextType.TEXT)
+       split_node = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+       expected = [
+        TextNode("This is a ", TextType.TEXT),
+        TextNode("text", TextType.BOLD),
+        TextNode(" node", TextType.TEXT),
+       ]
+       self.assertEqual(split_node, expected)
 
 if __name__ == "__main__":
     unittest.main()
