ed6122e489f4c656920e12aa3351e0d39f127b22
Julia Bush <j.elizabethbush@gmail.com>
2025-12-11 12:24:40 +0100

    add: adding a colon

--- src/test_extract.py
+++ src/test_extract.py
@@ -1,8 +1,8 @@
 import unittest
 
 class TestExtractMarkdown(unittest.TestCase):
-def test_extract_markdown_images(self):
-    matches = extract_markdown_images(
-        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
     )
     self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
