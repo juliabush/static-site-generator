1a3d53d036ce9bf515aadc39ca8e208b8c0c3fc5
Julia Bush <j.elizabethbush@gmail.com>
2025-12-26 21:38:40 +0100

    add: filling out the text_to_textnodes function

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from htmlnode import LeafNode
+from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TextType(Enum):
     TEXT = "text"
@@ -9,6 +10,7 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -37,3 +39,12 @@ def text_node_to_html_node(text_node):
         if text_node.text_type == TextType.IMAGE:
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         raise ValueError(f"invalid text type: {text_node.text_type}")
+    
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
