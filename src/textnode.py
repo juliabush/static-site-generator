daf09efa38a48446597051628c3401156edf10e7
Julia Bush <j.elizabethbush@gmail.com>
2025-12-26 21:42:36 +0100

    remove: old text to node function

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 from htmlnode import LeafNode
-from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TextType(Enum):
     TEXT = "text"
@@ -39,12 +38,3 @@ def text_node_to_html_node(text_node):
         if text_node.text_type == TextType.IMAGE:
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         raise ValueError(f"invalid text type: {text_node.text_type}")
-    
-def text_to_textnodes(text):
-    nodes = [TextNode(text, TextType.TEXT)]
-    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
-    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
-    nodes = split_nodes_image(nodes)
-    nodes = split_nodes_link(nodes)
-    return nodes
