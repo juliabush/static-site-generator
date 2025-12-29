ac0900ce9b13d436540cde6b4f9b90d1290cd9ec
Julia Bush <j.elizabethbush@gmail.com>
2025-12-29 22:00:05 +0100

    update: changing logic to markdown python file

--- src/markdown_to_block.py
+++ src/markdown_to_block.py
@@ -1,9 +1,10 @@
 def markdown_to_blocks(markdown):
-    block_list = []
-    block = markdown.split("\n\n")
-    stripped_block = block.strip()
-    if len(stripped_block) == 0:
-        pass
-    else:
-        block_list.append(block)
-    return block_list
+    result = []
+    block_list = markdown.split("\n\n")
+    for block in block_list:
+        stripped_block = block.strip()
+        if len(stripped_block) == 0:
+            pass
+        else:
+            result.append(stripped_block)
+    return result
