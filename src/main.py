2da4e83ea2be861447032f4f10e10e21d0a06f20
Julia Bush <j.elizabethbush@gmail.com>
2025-12-30 15:12:44 +0100

    add: defining and calling the recursive copy function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import shutil
+import os
 from textnode import TextNode, TextType
 
 def main():
@@ -6,3 +8,20 @@ def main():
 
 
 main()
+
+
+def recursive_copy(source_path, destination_path):
+    if os.path.exists(destination_path):
+        shutil.rmtree(destination_path)
+    os.mkdir(destination_path)
+    for file_path in os.listdir(source_path):
+        extended_path = os.path.join(source_path, file_path)
+        if os.path.isfile(extended_path) == True:
+            shutil.copy(extended_path, destination_path)
+        else:
+            # final_path = os.path.join(source_path, file_path)
+            full_source = extended_path
+            full_destination = os.path.join(destination_path, file_path)
+            recursive_copy(full_source, full_destination)
+
+recursive_copy("static", "public")
