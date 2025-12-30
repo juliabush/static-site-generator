81f1d463ed80bead29da40333cc8e5dfe15de3b2
Julia Bush <j.elizabethbush@gmail.com>
2025-12-30 16:26:23 +0100

    add: test for extract markdown title

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -7,3 +7,14 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("#"):
+            parts = line.split("# ")
+            title_part = parts[1]
+            clean_title = title_part.strip()
+            return clean_title
+        
+    raise Exception("No header in markdown")
