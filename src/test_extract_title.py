21af93318a38eca8c28b4bbc393674032c2f4b4f
Julia Bush <j.elizabethbush@gmail.com>
2025-12-30 16:25:48 +0100

    add: extract title function to get title from markdown

---
+++ src/test_extract_title.py
@@ -0,0 +1,21 @@
+import unittest
+
+from extract_markdown import extract_title
+
+class TestExtractTitle(unittest.TestCase):
+    def test_title_extraction(self):
+        md = "This is markdown\n# Heading\n Goodbye"
+        title_extraction = extract_title(md)
+        self.assertEqual(
+            title_extraction,
+            "Heading"
+        )
+    
+    def test_title_extraction_exception(self):
+        md = "This is markdown\n Goodbye"
+        with self.assertRaises(Exception):
+            extract_title(md)
+
+
+if __name__ == "__main__":
+    unittest.main()
