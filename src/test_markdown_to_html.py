e21b7e7d1564115b541e5f56c58b4ba286d18635
Julia Bush <j.elizabethbush@gmail.com>
2025-12-30 12:45:23 +0100

    add: text for markdwon to html function

---
+++ src/test_markdown_to_html.py
@@ -0,0 +1,25 @@
+import unittest
+from markdown_to_html import markdown_to_html_node
+from htmlnode import HTMLNode, LeafNode
+
+class TestMarkdownToHTMLNode(unittest.TestCase):
+    def test_paragraph_conversion(self):
+        markdown_input = "This is a simple paragraph."
+        expected_html_node = HTMLNode(
+            tag="div",
+            children=[
+                HTMLNode(
+                    tag="p",
+                    children=[
+                        LeafNode(None, "This is a simple paragraph.")
+                    ]
+                )
+            ]
+        )
+
+        result_node = markdown_to_html_node(markdown_input)
+
+        self.assertEqual(result_node.tag, expected_html_node.tag)
+        self.assertEqual(result_node.children[0].tag, expected_html_node.children[0].tag)
+        self.assertEqual(result_node.children[0].children[0].value, expected_html_node.children[0].children[0].value)
+
