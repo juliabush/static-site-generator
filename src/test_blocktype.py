41919821f2a7465063496672a2da4b858cfd21ed
Julia Bush <j.elizabethbush@gmail.com>
2026-01-03 16:58:42 +0100

    add: test for blockType function

---
+++ src/test_blocktype.py
@@ -0,0 +1,17 @@
+import unittest
+
+from blocktype import block_to_block_type, BlockType
+
+class TestBlockType(unittest.TestCase):
+    def test_block_to_block_type_heading():
+        block = "# My heading"
+        assert block_to_block_type(block) == BlockType.HEADING
+
+        block2 = "- Hello\n- Bye\n- Goodnight"
+        assert block_to_block_type(block2) == BlockType.UNORDERED_LIST
+
+        block3 = "manchild, why you always come runnin to me"
+        assert block_to_block_type(block3) == BlockType.PARAGRAPH
+
+        block4 = "1. Hello\n2. Goodbye"
+        assert block_to_block_type(block4) == BlockType.ORDERED_LIST
